0572cb90479f72027eab60825b71bc5e9acc2882
Matheus Barbiot <matheusbarbiot01@gmail.com>
2023-03-29 23:03:19 -0300

    finalização da logica

--- Jogo da Forca.py
+++ Jogo da Forca.py
@@ -1,3 +1,5 @@
+from Contlist import contlist, contl
+
 def Llist(list):
     for L in list:
         print(L, end='')
@@ -5,27 +7,38 @@ def Llist(list):
 
 #   INICIO:
 PALAVRA_ = str(input('Digite a palavra da forca: '))
-ACERTOS_ = len(PALAVRA_)
 CHANCES_ = 6
 BACKS_ = '_' * len(PALAVRA_)
 BACKS_n = []
-
+PALAVRA_n = []
+ACERTOS_ = int(len(PALAVRA_))
 for B_ in BACKS_:
     BACKS_n.append(B_)
 
+for l in PALAVRA_:
+    PALAVRA_n.append(l)
+
 Llist(BACKS_n)
 while ACERTOS_ != 0:
+
     CHUTE_ = str(input('Chute uma letra: '))[0]
+    Z_ = contlist(PALAVRA_n, CHUTE_)
 
+    if CHUTE_ in PALAVRA_n:
+        if Z_ > 1:
+            for P in range(0, Z_):
+                POS_ = int(PALAVRA_n.index(CHUTE_))
+                BACKS_n[POS_] = PALAVRA_n[POS_]
+                PALAVRA_n[POS_] = '-'
+            Llist(BACKS_n)
+            ACERTOS_ -= Z_
 
-#   CONDIÇÃO:
-
-    if CHUTE_ in PALAVRA_:
-
-        POS_ = int(PALAVRA_.find(CHUTE_))
-        BACKS_n[POS_] = PALAVRA_[POS_]
-        ACERTOS_ -= 1
-        Llist(BACKS_n)
+        else:
+            POS_ = int(PALAVRA_n.index(CHUTE_))
+            BACKS_n[POS_] = PALAVRA_n[POS_]
+            PALAVRA_n[POS_] = '-'
+            ACERTOS_ -= 1
+            Llist(BACKS_n)
     else:
         if CHANCES_ > 1:
             CHANCES_ -= 1
@@ -33,3 +46,4 @@ while ACERTOS_ != 0:
         else:
             print('Você Perdeu')
             break
+#   CONDIÇÃO:
