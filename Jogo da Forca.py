0c4ec1bca8a9472ecaab45edd10fe7b53f654c04
Matheus Barbiot <matheusbarbiot01@gmail.com>
2023-03-29 21:34:19 -0300

    Condicionando

--- Jogo da Forca.py
+++ Jogo da Forca.py
@@ -2,7 +2,24 @@
 PALAVRA_ = str(input('Digite a palavra da forca: '))
 CHANCES_ = 6
 BACKS_ = '_' * len(PALAVRA_)
+BACKS_n = []
 
 for B_ in BACKS_:
-    print(B_ + ' ', end='')
+    BACKS_n.append(B_)
 
+print(BACKS_n)
+
+CHUTE_ = str(input('Chute uma letra: '))[0]
+
+
+#   CONDIÇÃO:
+if CHUTE_ in PALAVRA_:
+
+    POS_ = int(PALAVRA_.find(CHUTE_))
+    BACKS_n[POS_] = PALAVRA_[POS_]
+
+    print(POS_)
+    print(BACKS_n)
+else:
+    CHANCES_ -= 1
+    print(f'ERRADO. Chanches: {CHANCES_}')
