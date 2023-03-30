c56760a5d71bf7b1d6cc86a9656eec470e3dd163
Matheus Barbiot <matheusbarbiot01@gmail.com>
2023-03-29 21:49:02 -0300

    Alterações Basicas

--- Jogo da Forca.py
+++ Jogo da Forca.py
@@ -1,5 +1,11 @@
+def Llist(list):
+    for L in list:
+        print(L, end='')
+    print('')
+
 #   INICIO:
 PALAVRA_ = str(input('Digite a palavra da forca: '))
+ACERTOS_ = len(PALAVRA_)
 CHANCES_ = 6
 BACKS_ = '_' * len(PALAVRA_)
 BACKS_n = []
@@ -7,19 +13,23 @@ BACKS_n = []
 for B_ in BACKS_:
     BACKS_n.append(B_)
 
-print(BACKS_n)
-
-CHUTE_ = str(input('Chute uma letra: '))[0]
+Llist(BACKS_n)
+while ACERTOS_ != 0:
+    CHUTE_ = str(input('Chute uma letra: '))[0]
 
 
 #   CONDIÇÃO:
-if CHUTE_ in PALAVRA_:
 
-    POS_ = int(PALAVRA_.find(CHUTE_))
-    BACKS_n[POS_] = PALAVRA_[POS_]
+    if CHUTE_ in PALAVRA_:
 
-    print(POS_)
-    print(BACKS_n)
-else:
-    CHANCES_ -= 1
-    print(f'ERRADO. Chanches: {CHANCES_}')
+        POS_ = int(PALAVRA_.find(CHUTE_))
+        BACKS_n[POS_] = PALAVRA_[POS_]
+        ACERTOS_ -= 1
+        Llist(BACKS_n)
+    else:
+        if CHANCES_ > 1:
+            CHANCES_ -= 1
+            print(f'ERRADO. Chances: {CHANCES_}')
+        else:
+            print('Você Perdeu')
+            break
